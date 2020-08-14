7f49f955d883d050d14deca328afea85b22b8715
lele <lr18516312@163.com>
2020-08-14 20:45:07 +0800

    [roomserver] update

--- TankBattle/src/common/struct.go
+++ TankBattle/src/common/struct.go
@@ -1,5 +1,12 @@
 package common
 
+type MsgType uint16
+
+const (
+	MsgType_Token MsgType = 0
+	MsgType_Move  MsgType = 1
+)
+
 // 客户请求
 type ReqGetIDMsg struct {
 	DeviceId string `json:"deviceId"`
--- TankBattle/src/roomserver/room.go
+++ TankBattle/src/roomserver/room.go
@@ -69,6 +69,11 @@ func (this *Room) Start() {
 
 func (this *Room) GameLoop() {
 	for i := 0; i < 30; i++ {
+		// SceneMsg, 用于同步场景，在球球里面是100ms一次，在该游戏中，SceneMsg可以包含
+		// 以下信息：当前视野、当前视野内的玩家位置（包括自己）、当前视野内的子弹位置
+
+		// 对于单次处理时间内玩家操作过多的问题，顺序处理即可，如果出现玩家卡顿的现象，再考虑优化
+		// 例如给玩家的操作添加帧号，但是这个是可选项，不是必要的
 		time.Sleep(time.Duration(1) * time.Second)
 		glog.Info("game is running")
 	}
