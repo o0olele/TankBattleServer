14b953c6cf3d249908d28b3d7602f977bc3b04c5
lele <lr18516312@163.com>
2020-08-14 15:13:13 +0800

    [http] some handlers

--- TankBattle/src/logicserver/httpserver.go
+++ TankBattle/src/logicserver/httpserver.go
@@ -4,47 +4,16 @@ import (
 	"base/env"
 	"encoding/json"
 	"fmt"
-	"net"
 	"net/http"
 	"time"
 
 	common "common"
 
-	"github.com/garyburd/redigo/redis"
 	"github.com/golang/glog"
+	"github.com/rs/cors"
 )
 
-var pool *redis.Pool
-var listkey = "ListKey"
-
-// 连接池
-func newPool(server, password string) *redis.Pool {
-	return &redis.Pool{
-		MaxIdle:     64,
-		MaxActive:   1000,
-		IdleTimeout: 240 * time.Second,
-		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", server)
-			if err != nil {
-				return nil, err
-			}
-			return c, err
-		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			if time.Since(t) < time.Minute {
-				return nil
-			}
-			_, err := c.Do("PING")
-			return err
-		},
-	}
-}
-
 func GetNameHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	//w.Header().Set("Content-Type", "application/json")
 	randName := "NickName" + GetDateFormat()
 
 	fmt.Fprintf(w, randName)
@@ -61,63 +30,40 @@ func GetNameHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetIDHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	//w.Header().Set("Content-Type", "application/json")
+	msg := common.ReqGetIDMsg{
+		DeviceId: r.FormValue("DeviceId"),
+		Ip:       r.FormValue("Ip"),
+	}
 
-	var msg common.ReqGetIDMsg
+	glog.Info("[login] Get msg", msg)
 
-	err := json.NewDecoder(r.Body).Decode(&msg)
+	id, err := AccRedis_GetMe().GetIncID()
 	if nil != err {
-		glog.Error("[login] Get id fail")
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		glog.Error("[login] Get Inc Id Fail ", err)
 		return
 	}
 
 	err = json.NewEncoder(w).Encode(&common.RetGetIDMsg{
-		Id: 10001,
+		Id: id,
 	})
 	if nil != err {
-		glog.Error("[login] Return id fail")
+		glog.Error("[login] Return id Fail ", err)
+		return
 	}
 
-	GetVailabelRoomInfo("123456789")
+	conn := AccRedis_GetMe().pool.Get()
+	defer conn.Close()
+
+	_, err = conn.Do("SET", msg.DeviceId+":"+msg.Ip, id)
+	if nil != err {
+		glog.Error("[login] Set Userinfo Fail ", err)
+		return
+	}
+
 }
 
 func GetRoomHandler(w http.ResponseWriter, r *http.Request) {
-	if pool != nil {
-		conn := pool.Get()
-		defer conn.Close()
-
-		listLen, err := redis.Int(conn.Do("LLEN", listkey))
-		if err != nil {
-			fmt.Println("Redis LLEN error!")
-			return
-		}
-
-		values, err := redis.MultiBulk(conn.Do("LRANGE", listkey, 0, listLen))
-		items, err := redis.Strings(values, nil)
-		for _, s := range items {
-			fmt.Fprintf(w, s+"\n")
-		}
-
-	}
-
-}
-
-func ClearAllHandler(w http.ResponseWriter, r *http.Request) {
-	if pool != nil {
-		conn := pool.Get()
-		defer conn.Close()
-
-		_, err := conn.Do("DEL", listkey)
-		if err != nil {
-			fmt.Println("Redis DEL error!")
-			return
-		} else {
-			fmt.Fprintf(w, "clear success!")
-		}
-	}
+	GetVailabelRoomInfo("123456789")
 }
 
 // 时间戳转年月日 时分秒
@@ -126,21 +72,19 @@ func GetDateFormat() string {
 }
 
 func StartHttpServer() bool {
-	pool = newPool("localhost:6379", "")
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+	})
 
-	http.HandleFunc("/getname", GetIDHandler)
-	http.HandleFunc("/getroom", GetRoomHandler)
-	http.HandleFunc("/getid", GetIDHandler)
-	http.HandleFunc("/clean", ClearAllHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/getname", GetIDHandler)
+	mux.HandleFunc("/getroom", GetRoomHandler)
+	mux.HandleFunc("/getid", GetIDHandler)
 
 	addr := env.Get("logic", "listen")
-	ln, err := net.Listen("tcp", addr)
-	if nil != err {
-		glog.Error("[Start] Bind Port Error, Port=", addr, ",", err)
-		return false
-	}
+	handler := c.Handler(mux)
+	http.ListenAndServe(addr, handler)
 
-	go http.Serve(ln, nil)
 	glog.Info("[Start] Bind Port Success, Port=", addr)
 
 	return true
