561fa2e5f72ac2a0206dc142296151bf8f9d7e72
lele <lr18516312@163.com>
2020-09-03 11:59:15 +0800

    [rpc] update

--- TankBattle/src/logicserver/grpclogic.go
+++ TankBattle/src/logicserver/grpclogic.go
@@ -27,3 +27,89 @@ func GetVailabelRoomInfo(token string) (*proto.ConnectRoomInfo, error) {
 
 	return result.MInfo, nil
 }
+
+// type RPCClient struct {
+// 	client  *rpc.Client
+// 	isclose int32
+// 	addr    string
+// }
+
+// var mRPCClient *RPCClient
+
+// func RPCClient_GetMe() *RPCClient {
+// 	if nil == mRPCClient {
+// 		mRPCClient = &RPCClient{
+// 			isclose: 1,
+// 		}
+// 	}
+
+// 	return mRPCClient
+// }
+
+// func (this *RPCClient) Init(addr string) bool {
+// 	if "" == addr {
+// 		return false
+// 	}
+
+// 	this.addr = addr
+// 	return this.Connect()
+// }
+
+// func (this *RPCClient) Connect() bool {
+// 	if 0 == atomic.LoadInt32(&this.isclose) {
+// 		return false
+// 	}
+
+// 	client, err := rpc.Dial("tcp", this.addr)
+// 	if nil != err {
+// 		return false
+// 	}
+
+// 	if nil != client {
+// 		this.client = client
+// 	}
+
+// 	atomic.StoreInt32(&this.isclose, 0)
+// 	return true
+// }
+
+// func (this *RPCClient) ReConnect() bool {
+// 	if atomic.CompareAndSwapInt32(&this.isclose, 0, 1) {
+// 		for {
+// 			if this.Connect() {
+// 				break
+// 			}
+// 			time.Sleep(time.Second)
+// 		}
+// 		return true
+// 	}
+// 	return false
+// }
+
+// func (this *RPCClient) RemoteCall(name string, args interface{}, reply interface{}) error {
+// 	if nil == this.client {
+// 		return errors.New("error")
+// 	}
+
+// 	err := this.client.Call(name, args, reply)
+// 	if nil != err {
+// 		if this.ReConnect() {
+// 			return this.client.Call(name, args, reply)
+// 		} else {
+// 			for i := 0; i < 30; i++ {
+// 				if nil == this.client {
+// 					continue
+// 				}
+
+// 				err = this.client.Call(name, args, reply)
+// 				if err != rpc.ErrShutdown {
+// 					break
+// 				}
+// 			}
+// 		}
+
+// 		return err
+// 	}
+
+// 	return nil
+// }
